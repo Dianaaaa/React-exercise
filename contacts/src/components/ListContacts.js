010df00aea560f73e50da8dd78e91230f7d1ab4f
Dianaaaa <1035254583@qq.com>
2019-04-01 20:58:31 +0800

    add the admin search component

--- contacts/src/components/ListContacts.js
+++ contacts/src/components/ListContacts.js
@@ -88,18 +88,18 @@ class ListContacts extends Component {
                 <ol className='contact-list'>
                     {showedContacts.map((contact) => (
                         <li key={contact.id} className='contact-list-item'>
-                        <div className='contact-avatar'
-                            style = {{
-                                backgroundImage: `url(${contact.avatarURL})` 
-                            }}
-                        ></div>
-                        <div className='contact-details'>
-                            <p>{contact.name}</p>
-                            <p>{contact.handle}</p>
-                        </div>
-                        <button className='contact-remove'
-                            onClick={() => onDeleteContact(contact)}
-                        ></button>
+                            <div className='contact-avatar'
+                                style = {{
+                                    backgroundImage: `url(${contact.avatarURL})` 
+                                }}
+                            ></div>
+                            <div className='contact-details'>
+                                <p>{contact.name}</p>
+                                <p>{contact.handle}</p>
+                            </div>
+                            <button className='contact-remove'
+                                onClick={() => onDeleteContact(contact)}
+                            ></button>
                         </li>
                     ))}
                 </ol>
